d737f99ef706bca918792a3959d97779a6357e3b
andrei.rajkovic <andrei.rajkovic@berkeleylights.com>
2024-03-10 22:08:52 -0700

    hardcode the file name used for the sgdp variant container

--- igm_churchill_ancestry/utilities/vcf2sparse.py
+++ igm_churchill_ancestry/utilities/vcf2sparse.py
@@ -1,5 +1,4 @@
 from igm_churchill_ancestry.utilities.utilities import genotype_dictionary
-import os
 import glob
 import json
 import warnings
@@ -15,10 +14,13 @@ def vcf_to_json(parsed_vcf, attribute_dir, locus_converter_json_path):
 
     Returns a JSON filled with the VCF data
     """
-    gnomad_json = glob.glob(attribute_dir + '/*.json')[0]
+    if 'sgdp' in attribute_dir:
+        variants_of_interest = attribute_dir + 'sgdp.intersect_exome.sparse_matrix.var_ids.json'
+    else:
+        variants_of_interest = glob.glob(attribute_dir + '/*.json')[0]
     try:
-        with open(gnomad_json, 'r') as myfile:
-            gnomad_container = json.load(myfile)
+        with open(variants_of_interest, 'r') as myfile:
+            variant_container = json.load(myfile)
     except Exception as e:
         print(f"Failed to load gnomAD JSON error: {e}")
         return
@@ -46,7 +48,7 @@ def vcf_to_json(parsed_vcf, attribute_dir, locus_converter_json_path):
         # convert the locus if we are using locus vcf
         if locus_converter is not None and locus_id in locus_converter:
             locus_id = locus_converter[locus_id]
-        if locus_id in gnomad_container:
+        if locus_id in variant_container:
             try:
                 genotype = values[9][:3:]
             except Exception as e:
@@ -58,37 +60,12 @@ def vcf_to_json(parsed_vcf, attribute_dir, locus_converter_json_path):
             except Exception as e:
                 print(f"Unknown genotype: {genotype}. {e}")
                 return
-            gnomad_container[locus_id] = genotype_int
+            variant_container[locus_id] = genotype_int
     if gt_sum > 0:
-        return gnomad_container
+        return variant_container
     else:
         warnings.warn("No genotypes present. Check to make sure VCF has variants")
-        return gnomad_container
-
-
-def load_snp_order(attribute_dir):
-    """
-    A function to read in the order of the SNPs
-    as they were used in the generation of the
-    gradient boosted decision tree model. This
-    is critical in order to feed the model
-    the correct input data. Any change in order
-    will create a conflict and either cause xgb
-    to fail or incorrect predictions will be
-    generated.
-    """
-
-    ordered_snps_path = glob.glob(attribute_dir + '/*.txt')[0]
-    o_snps = []
-    try:
-        with open(ordered_snps_path, 'r') as fin:
-            for line in fin:
-                line = line.strip()
-                o_snps.append(line)
-        return o_snps
-    except Exception as e:
-        print(f'File path probably incorrect {ordered_snps_path}. {e}')
-        return
+        return variant_container
 
 
 def json_to_sparse_matrix(g_container, o_snps):
